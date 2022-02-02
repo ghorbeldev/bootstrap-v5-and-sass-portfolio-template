5cd6a9f4c4454f5023ec47474b84a0e80da92fd1
ghorbeldev <adem.ghorbel9@gmail.com>
2022-02-02 19:31:37 +0100

    ''

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,13 +13,33 @@ portfolioButtons.forEach(btn => {
 });
 const sleep = ms => new Promise(res => setTimeout(res, ms));
 const setActiveCategory = category => {
-	portfolioBoxs.forEach(async box => {
+	portfolioBoxs.forEach(box => {
 		if (category === 'all') {
-			box.classList.remove('disable');
-		} else if (box.dataset.category !== category) {
-			box.classList.add('disable');
+			show(box);
+		} else if (box.dataset.category === category) {
+			show(box);
 		} else {
-			box.classList.remove('disable');
+			hide(box);
 		}
 	});
 };
+const hide = function (elm, ms = 500) {
+	var y = elm.style ? elm.style.display : '';
+	if (!y || y == 'block') {
+		// change the opacity. CSS will handle the animation.
+		elm.style.opacity = '0';
+		elm.style.transform = 'translateX(-40%)';
+		// change the display to "none" after a delay.
+		setTimeout(function () {
+			elm.style.display = 'none';
+		}, ms);
+	}
+};
+const show = function (elm) {
+	elm.style.display = 'block';
+	setTimeout(function () {
+		elm.style.transform = '';
+		elm.style.opacity = '1';
+	}, 10);
+};
+// hide(portfolioBoxs[0]);
