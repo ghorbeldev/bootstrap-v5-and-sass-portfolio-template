b4c4dd44146a16e33508700887b6a1e29a9f6ffe
ghorbeldev <adem.ghorbel9@gmail.com>
2022-02-05 13:07:38 +0100

    add custom scrollbar && edit the UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,35 +11,13 @@ portfolioButtons.forEach(btn => {
 		setActiveCategory(btn.dataset.category);
 	};
 });
-const sleep = ms => new Promise(res => setTimeout(res, ms));
 const setActiveCategory = category => {
 	portfolioBoxs.forEach(box => {
-		if (category === 'all') {
-			show(box);
-		} else if (box.dataset.category === category) {
-			show(box);
-		} else {
-			hide(box);
-		}
+		box.style =
+			category === 'all'
+				? ''
+				: box.dataset.category === category
+				? ''
+				: 'display:none';
 	});
 };
-const hide = function (elm, ms = 500) {
-	var y = elm.style ? elm.style.display : '';
-	if (!y || y == 'block') {
-		// change the opacity. CSS will handle the animation.
-		elm.style.opacity = '0';
-		elm.style.transform = 'translateX(-40%)';
-		// change the display to "none" after a delay.
-		setTimeout(function () {
-			elm.style.display = 'none';
-		}, ms);
-	}
-};
-const show = function (elm) {
-	elm.style.display = 'block';
-	setTimeout(function () {
-		elm.style.transform = '';
-		elm.style.opacity = '1';
-	}, 10);
-};
-// hide(portfolioBoxs[0]);
